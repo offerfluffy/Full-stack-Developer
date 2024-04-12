41b54c837de26734bac7a1a7870dcea1131a7033
Kyrylo <kirill.shelydchenko@gmail.com>
2024-04-12 18:16:24 +0300

    commit

---
+++ Front-end/HTML CSS/EPAM/Homework 6/script.js
@@ -0,0 +1,14 @@
+const login = prompt("Enter your login:");
+const password = "1261";
+
+if (login == "Admin") {
+  const userPassword = prompt("Enter your password:");
+
+  if (userPassword === password) {
+    alert("Welcome");
+  } else {
+    alert("Invalid password");
+  }
+} else {
+  alert("Access denied");
+}
